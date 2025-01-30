63c1b786b5c3656b24090f18b26da9ed9a71fc63
Priyanka <priyankashah3107@gmail.com>
2025-01-30 23:01:19 +0530

    feat: generateTokenAndSetCookie

--- app/api/(v1)/(user)/signup/route.ts
+++ app/api/(v1)/(user)/signup/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import { z } from "zod";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
@@ -43,12 +44,12 @@ export async function POST(request: Request) {
         { error: "Email Already Exist" },
         { status: 409 }
       );
-    }
+    } // already check this in database level
 
     // hash the password
 
-    const salt = await bcrypt.genSaltSync(10);
-    const hashedPassword = await bcrypt.hashSync(password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     // hash the password generate the salt
     // bcrypt the password and then login
@@ -65,13 +66,41 @@ export async function POST(request: Request) {
       },
     });
 
-    return NextResponse.json(
+    // const secret = process.env.JWT_SECRET || "sup3rS3c3t";
+    // const token = jwt.sign({ email, userId: user.id }, secret); // Include userId in the payload
+
+    // Generate JWT token
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+      throw new Error("JWT_SECRET is not defined in environment variables");
+    }
+
+    const token = jwt.sign({ email, userId: user.id }, secret, {
+      expiresIn: "30d", // Set token expiration
+    });
+
+    console.log("Token is", token);
+
+    const response = NextResponse.json(
       {
         message: "User created successfully",
         user: { email: user.email },
+        token,
       },
       { status: 201 }
     );
+
+    // Set cookie with proper method
+    response.headers.set(
+      "Set-Cookie",
+      `jwt=${token}; HttpOnly; Path=/; Max-Age=${
+        30 * 24 * 60 * 60
+      }; SameSite=Strict; ${
+        process.env.NODE_ENV !== "development" ? "Secure" : ""
+      }`
+    );
+
+    return response;
   } catch (error) {
     console.log(error);
     return NextResponse.json(
